4780c26620f8084a14d19530a9ee2f8cf6f54179
Rodion Gorkovenko <RodionGork@github.com>
2017-03-23 22:17:07 +0300

    HW1 - Counting task working

--- src/main/java/hw1rg/App.java
+++ src/main/java/hw1rg/App.java
@@ -6,11 +6,47 @@ import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class App {
+
+    private static class CompactString {
+        
+        private byte[] body;
+        private int hash;
+        
+        private CompactString(String s) {
+            body = s.getBytes();
+            hash = Arrays.hashCode(body);
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            return (o instanceof CompactString)
+                && Arrays.equals(((CompactString) o).body, body);
+        }
+        
+        @Override
+        public int hashCode() {
+            return hash;
+        }
+        
+        @Override
+        public String toString() {
+            return new String(body);
+        }
+        
+    }
     
     public static void main(String[] args) throws Exception {
         new App().run(args[0], args[1]);
@@ -20,7 +56,55 @@ public class App {
         Path inputPath = new Path(input);
         FileSystem fileSystem = FileSystem.get(inputPath.toUri(), new Configuration());
         List<Path> inputFiles = collectFilePaths(fileSystem, inputPath);
-        System.out.println(inputFiles);
+        Map<CompactString, AtomicLong> counters = new HashMap<>();
+        long skipped = 0;
+        long parsed = 0;
+        for (Path file : inputFiles) {
+            System.out.println("File: " + file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fileSystem.open(file)));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] parts = line.split("\\t");
+                if (parts.length != 21) {
+                    skipped += 1;
+                    continue;
+                }
+                parsed += 1;
+                CompactString id = new CompactString(parts[2]);
+                AtomicLong counter = counters.get(id);
+                if (counter == null) {
+                    counter = new AtomicLong();
+                    counters.put(id, counter);
+                    if (counters.size() % 10000 == 0) {
+                        System.out.println("Counters: " + counters.size());
+                    }
+                }
+                counter.incrementAndGet();
+            }
+        }
+        System.out.printf("Total lines parsed: %s, skipped: %s, unique ids: %s%n", parsed, skipped, counters.size());
+        long[] cnts = new long[counters.size()];
+        int i = 0;
+        for (AtomicLong v : counters.values()) {
+            cnts[i] = v.get();
+            i += 1;
+        }
+        Arrays.sort(cnts);
+        long threshold = cnts[cnts.length - 100];
+        List<Map.Entry<String, Long>> result = new ArrayList<>();
+        for (Map.Entry<CompactString, AtomicLong> e : counters.entrySet()) {
+            if (e.getValue().get() < threshold) {
+                continue;
+            }
+            result.add(new AbstractMap.SimpleEntry<>(e.getKey().toString(), e.getValue().get()));
+        }
+        Collections.sort(result, (x, y) -> y.getValue().compareTo(x.getValue()));
+        for (Map.Entry<String, Long> e : result) {
+            System.out.printf("%s: %s%n", e.getKey(), e.getValue());
+        }
     }
 
     private List<Path> collectFilePaths(FileSystem fileSystem, Path inputPath) throws IOException {
